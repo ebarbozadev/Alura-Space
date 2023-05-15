2e8705b9e17c53a4b75a8df169ae56db5c0c9f04
Emanuel Marcos dos Santos Barboza <emanuel@tigrara.local>
2023-05-15 17:26:53 -0300

    Adicionando arquivos

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,7 +1,28 @@
-import Cabecalho from "../componentes/Cabecalho";
+import Imagem from "./banner.png";
+import styles from "./PaginaInicial.module.scss";
 
-export default function PaginaInicial(){
-    return(
-        <Cabecalho />
+import Cabecalho from "componentes/Cabecalho";
+import Menu from "componentes/Menu";
+import Galeria from "componentes/Galeria";
+import Populares from "componentes/Populares";
+
+export default function PaginaInicial() {
+    return (
+        <>
+            <Cabecalho />
+            <main>
+                <section className={styles.principal}>
+                    <Menu />
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={Imagem} alt="A imagem da terra vista do espaço" />
+                    </div>
+                </section>
+                <div className={styles.galeria}>
+                    <Galeria />
+                    <Populares />
+                </div>
+            </main>
+        </>
     );
 }
